aa080f8216cfc85f949bcd84b8479c5b0d610f31
Cliff Click <cliffc@acm.org>
2013-08-10 12:52:41 -0700

    round 3? of drf
    Each node will build 1 tree at a time (concurrent with other nodes), and
    will be the master for that tree, pulling from an atomic tree list as it
    finishes its current tree.  Each tree is built on all nodes, but the
    master for a tree varies.  Each node maintains a local list of rows to
    be used for each tree; the local list is ordered in sections by tree
    split.  The tree-split bounds are kept on the master.  Local node work
    is limited to computing histograms over ranges of rows, or sorting a
    split into low & high halves.

--- src/main/java/hex/gbm/DRF.java
+++ src/main/java/hex/gbm/DRF.java
@@ -112,40 +112,230 @@ public class DRF extends Job {
     Timer t_tree = new Timer();
     System.out.println("Working on tree "+tree);
 
-    // Build the local set of rows.  We build histograms from local tree data,
-    // then roll up the histograms globally.  When building a histogram, we'll
-    // keep all the rows of a split adjacent in this rows array, and sorted.
-    long[] rows = buildLocalRows(fr.firstReadable());
+    // Ask all Nodes to setup for building this one tree, and hand me back a
+    // cookie (Key) to guide further tree building.
+    DRFTree root = new Start1Tree(fr).invokeOnAllNodes()._drf;
+    // Capture and reset root info
+    final Key[] keys = root._keys;   root._keys = null;
+    final int[] ends = root._begs;   root._begs = new int[ends.length];
+    System.out.println("ROOT="+root.toString(ends));
 
-    // build a tree-node 
-    
+    // Split the top-level tree based on random sampling.
+    // root._kids[0] will be sampled-in, and root._kids[1] will be sampled-out
+    new SampleSplit(root,keys,ends,sampleRate,seed).invokeOnAllNodes();
+    DRFTree in  = root._kids[0];
+    DRFTree out = root._kids[1];
+    System.out.println("ROOT2 IN ="+in .toString(out._begs));
+    System.out.println("ROOT2 OUT="+out.toString(ends));
 
+    // build a tree
+
+    // Remove local temp tree storage
+    Futures fs = new Futures();
+    for( Key k : keys )
+      UKV.remove(k,fs);
+    fs.blockForPending();
     System.out.println("Done on tree "+tree+" in "+t_tree);
   }
 
-  // --------------------------------------------------------------------------
-  private long[] buildLocalRows(Vec v0) {
-    long sum=0;
-    int nchunks = v0.nChunks();
-    for( int i=0; i<nchunks; i++ )
-      if( v0.chunkKey(i).home() )
-        sum += v0.chunk2StartElem(i+1)-v0.chunk2StartElem(i);
-    System.out.println("Found "+sum+" local rows ");
-    if( sum >= Integer.MAX_VALUE ) throw H2O.unimpl(); // More than 2bil rows per node?
+  // The simple tree-node class.  Every tree node has a list of local-rows that
+  // landed on that tree node; the list is large and kept in the LocalTree
+  // structure on each JVM.  We only keep the start & end region marker in the
+  // large LocalTree here.
+  private static class DRFTree extends Iced {
+    Key [] _keys;
+    int [] _begs;
+    DRFTree[] _kids;
 
-    // Get space for 'em
-    long[] rows = MemoryManager.malloc8((int)sum);
+    // Called to make a Root DRFTree.  Passed in a key, and the length of the
+    // entire row list.  Sets the Key list.
+    DRFTree( Key key, int end ) { 
+      _keys = new Key[H2O.CLOUD.size()];
+      _begs = new int[_keys.length];
+      int idx = H2O.SELF.index();
+      _keys[idx] = key;
+      _begs[idx] = end;
+    }
+    // Merge trees on the Root DRFTree
+    void add1( DRFTree drf ) {
+      assert _kids==null;
+      for( int i=0; i<_keys.length; i++ )
+        if( drf._keys[i] != null ) {
+          _keys[i] = drf._keys[i];
+          _begs[i] = drf._begs[i];
+        }
+    }
 
-    // Fill in the rows this Node will work on
-    int k=0;
-    for( int i=0; i<nchunks; i++ )
-      if( v0.chunkKey(i).home() ) {
-        long end = v0.chunk2StartElem(i+1);
-        for( long j=v0.chunk2StartElem(i); j<end; j++ )
-          rows[k++] = j;
-      }    
-    
-    return rows;
+    // Called to make a new interior DRFTree.  Just a row start.
+    DRFTree( int beg ) { 
+      _begs = new int[H2O.CLOUD.size()];
+      int idx = H2O.SELF.index();
+      _begs[idx] = beg;
+    }
+    // Merge trees on interior DRFTree nodes.
+    // Merge all the kid-tree starts
+    void add2( DRFTree drf ) {
+      if( drf._kids == null ) return;
+      if( _kids == null ) _kids = drf._kids;
+      else for( int i=0; i<_kids.length; i++ )
+             _kids[i].add3(drf._kids[i]);
+    }
+    void add3( DRFTree drf ) {
+      assert _kids==null;
+      for( int i=0; i<_begs.length; i++ )
+        if( drf._begs[i] != 0 )
+          _begs[i] = drf._begs[i];
+    }
+
+    // Quick sanity check on bounds
+    boolean check( int[] ends ) {
+      for( int i=0; i<ends.length; i++ )
+        if( _begs[i] > ends[i] )
+          return false;
+      return true; 
+    }
+
+    // Pretty-print
+    @Override public String toString() {
+      String s = "{";
+      for( int i=0; i<_begs.length; i++ )
+        s += "["+H2O.CLOUD._memary[i]+","+_begs[i]+"],";
+      return s+"}";
+    }
+    // Pretty-print with the other ends
+    public String toString( int[] ends ) {
+      String s = "{";
+      for( int i=0; i<_begs.length; i++ )
+        s += "["+H2O.CLOUD._memary[i]+","+_begs[i]+","+ends[i]+"],";
+      return s+"}";
+    }
   }
 
+  // Start this one Tree on all Nodes.  Return a collection of Keys that
+  // uniquely identifies the tree being built on all Nodes.
+  private static class Start1Tree extends DRemoteTask<Start1Tree> {
+    final Frame _fr;
+    DRFTree _drf;
+    Start1Tree(Frame fr) { _fr = fr; }
+    @Override public void lcompute() {
+      // One Key per Tree, homed to self.
+      Key myKey = Key.make("T"+Key.make(),(byte)0,Key.BUILT_IN_KEY,H2O.SELF);
+      // Stash away local tree info on this Node, awaiting the next pass.
+      LocalTree lt = new LocalTree(myKey,_fr);
+      UKV.put(myKey,lt);
+      _drf = new DRFTree(myKey,lt._rows.length);
+      tryComplete();
+    }
+    // Combine Key/beg lists from across the cluster
+    @Override public void reduce(Start1Tree s1t) {
+      if( s1t == null ) return;
+      if( _drf == null ) _drf = s1t._drf;
+      else _drf.add1(s1t._drf);
+    }
+  }
+
+  // ---
+  private static abstract class Split extends DRemoteTask<Split> {
+    DRFTree _drf;               // The guy being split
+    int _ends[];                // Ends of split rows
+    abstract void doSplit(LocalTree lt, int beg, int end);
+    Split( DRFTree drf, Key[] keys, int[] ends ) { 
+      assert drf._keys == null; // These where cleaned out last pass
+      assert drf._kids == null; // No kids yet
+      assert drf.check(ends);   // Quicky bounds check
+      drf._keys = keys;         // Set in afresh
+      _drf  = drf; 
+      _ends = ends;
+    }
+    @Override public void lcompute() {
+      int idx = H2O.SELF.index();
+      LocalTree lt = DKV.get(_drf._keys[idx]).get();
+      int beg = _drf._begs[idx];
+      int end =      _ends[idx];
+      //assert lt.isOrdered(beg,end);
+      doSplit(lt,beg,end);
+      //assert lt.isOrdered(beg,end);
+      _drf._keys = null;        // No need to pass Keys back
+      _drf._begs = null;        // No need to pass begs back
+      _ends = null;             // No need to pass ends back
+      tryComplete();
+    }
+    // Combine Key/beg lists from across the cluster
+    @Override public void reduce(Split spl) {
+      if( spl == null ) return;
+      if( _drf == null ) _drf = spl._drf;
+      else _drf.add2(spl._drf);
+    }
+  }
+
+  // ---
+  private static class SampleSplit extends Split {
+    final double _sampleRate;
+    final long _seed;
+    SampleSplit( DRFTree root, Key[] keys, int[] ends, double sampleRate, long seed ) { 
+      super(root,keys,ends); 
+      _sampleRate = sampleRate;
+      _seed = seed;
+    }
+    void doSplit( LocalTree lt, int beg, int end ) {
+      assert beg == 0;          // Sampling is over the whole (local) tree
+      System.out.println("Splitting from "+beg+" to "+end);
+      long seed = _seed^end; // Determinstic tree seed, reseeded by rows
+      Random rand = new MersenneTwisterRNG(new int[]{(int)(seed>>32L),(int)seed});
+
+      // Sample over all rows, splitting the rows
+      int j=end;
+      for( int i=beg; i<j; i++ )
+        if( !(rand.nextFloat() <= _sampleRate) ) // Sample this row OUT
+          lt.swap(i,--j);
+
+      _drf._kids    = new DRFTree[2];
+      _drf._kids[0] = new DRFTree(beg);
+      _drf._kids[1] = new DRFTree( j );
+    }
+  }
+
+  // --------------------------------------------------------------------------
+  // The local top-level tree-info for one tree.
+  private static class LocalTree extends Iced {
+    final Key _key;             // A unique cookie for this datastructure
+    final Frame _fr;            // Frame for the tree being built
+    final long [] _rows;        // All the local datarows
+    LocalTree( Key key, Frame fr ) {
+      _key = key;
+      _fr = fr;
+      Vec v0 = fr.firstReadable();
+      long sum=0;
+      int nchunks = v0.nChunks();
+      for( int i=0; i<nchunks; i++ )
+        if( v0.chunkKey(i).home() )
+          sum += v0.chunk2StartElem(i+1)-v0.chunk2StartElem(i);
+      if( sum >= Integer.MAX_VALUE ) throw H2O.unimpl(); // More than 2bil rows per node?
+      
+      // Get space for 'em
+      _rows = MemoryManager.malloc8((int)sum);
+      
+      // Fill in the rows this Node will work on
+      int k=0;
+      for( int i=0; i<nchunks; i++ )
+        if( v0.chunkKey(i).home() ) {
+          long end = v0.chunk2StartElem(i+1);
+          for( long j=v0.chunk2StartElem(i); j<end; j++ )
+            _rows[k++] = j;
+      }    
+      assert isOrdered(0,_rows.length);
+    }
+    void swap( int i, int j ) { long tmp = _rows[i]; _rows[i] = _rows[j]; _rows[j] = tmp; }
+
+    private boolean isOrdered( int beg, int end ) {
+      if( beg == end ) return true;
+      long r = _rows[beg];
+      for( int i = beg+1; i<end; i++ ) {
+        long s = _rows[i];
+        if( r > s ) return false;
+        r = s;
+      }
+      return true;
+    }
+  }
 }
